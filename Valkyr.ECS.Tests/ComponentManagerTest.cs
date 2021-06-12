75dd0240493ac0d6ebef554d62ff3adae1573c2a
Sebastian Berning <hunter.Vasitas@googlemail.com>
2021-06-12 12:29:55 +0200

    Valkyr.ECS: Added a static component manager to allow easy access to the required pool. Component Manager can serve multiple worlds.

---
+++ Valkyr.ECS.Tests/ComponentManagerTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Valkyr.ECS.Tests
+{
+  public class ComponentManagerTest
+  {
+    [Fact]
+    public void GetOrCreate_ValidWorldIdAndMaxCapacity_ValidPoolReturned()
+    {
+      short worldId = 1;
+      int maxCapacity = 1;
+
+      ComponentManager<UnittestComponent>.GetOrCreate(worldId, maxCapacity).Should().NotBeNull();
+    }
+  }
+}
---
+++ Valkyr.ECS/ComponentManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Concurrent;
+
+namespace Valkyr.ECS
+{
+  internal static class ComponentManager<T>
+    where T : struct
+  {
+    private static readonly ConcurrentDictionary<short, IPool<T>> pools = new();
+
+    internal static IPool<T> GetOrCreate(short worldId, int maxCapacity)
+    {
+      return pools.GetOrAdd(worldId, new Pool<T>(maxCapacity));
+    }
+  }
+}
