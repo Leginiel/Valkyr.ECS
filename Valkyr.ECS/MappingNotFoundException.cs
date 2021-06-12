6f258d6bad3d7534e189233b50ae4cb591643845
Sebastian Berning <hunter.Vasitas@googlemail.com>
2021-06-12 12:28:39 +0200

    Valkyr.ECS: Adding custom exceptions that are thrown from pool.

---
+++ Valkyr.ECS/MappingNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Valkyr.ECS
+{
+  public class MappingNotFoundException : Exception
+  {
+    public MappingNotFoundException(int id)
+      : base($"No Mapping exists for Id {id}") { }
+  }
+}
---
+++ Valkyr.ECS/MaximumCapacityReachedException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Valkyr.ECS
+{
+  public class MaximumCapacityReachedException : Exception
+  {
+    public MaximumCapacityReachedException(int maxCapacity)
+    : base($"Maximum capacity of {maxCapacity} for storage reached") { }
+  }
+}
