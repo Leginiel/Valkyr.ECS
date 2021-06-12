5101dc5bf4e0ec2fb3d1397552d4511cee6aba44
Sebastian Berning <hunter.Vasitas@googlemail.com>
2021-06-12 21:28:31 +0200

    ValkyrECS: Added Source generator for components. The source generator generates automatically components based on the the declarative attribute on fields.

---
+++ Valkyr.ECS.Generator/FieldAttributeSyntaxReceiver.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Valkyr.ECS.Generator
+{
+  internal class FieldAttributeSyntaxReceiver : ISyntaxContextReceiver
+  {
+    public List<IFieldSymbol> Fields { get; } = new List<IFieldSymbol>();
+
+    public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
+    {
+      if (context.Node is FieldDeclarationSyntax fieldDeclarationSyntax
+          && fieldDeclarationSyntax.AttributeLists.Count > 0)
+      {
+        foreach (VariableDeclaratorSyntax variable in fieldDeclarationSyntax.Declaration.Variables)
+        {
+          IFieldSymbol fieldSymbol = context.SemanticModel.GetDeclaredSymbol(variable) as IFieldSymbol;
+          if (fieldSymbol.GetAttributes().Any(ad => ad.AttributeClass.ToDisplayString() == "Valkyr.ECS.ComponentAttribute"))
+          {
+            Fields.Add(fieldSymbol);
+          }
+        }
+      }
+    }
+  }
+}
---
+++ Valkyr.ECS/ComponentAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Valkyr.ECS
+{
+  [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+  public sealed class ComponentAttribute : Attribute
+  {
+    public string Name { get; }
+    public ComponentAttribute(string name)
+    {
+      Name = name;
+    }
+  }
+}
