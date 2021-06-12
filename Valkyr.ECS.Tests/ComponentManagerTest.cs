d071d64507c4d26f3dc6a17c2301dbf6dfb0b076
Sebastian Berning <hunter.Vasitas@googlemail.com>
2021-06-12 18:15:43 +0200

    Valkyr.ECS: Refactor ComponentManager to support destruction of all world related components.

--- Valkyr.ECS.Tests/ComponentManagerTest.cs
+++ Valkyr.ECS.Tests/ComponentManagerTest.cs
@@ -10,8 +10,21 @@ namespace Valkyr.ECS.Tests
     {
       short worldId = 1;
       int maxCapacity = 1;
+      ComponentManager<UnittestComponent> componentManager = ComponentManager.Instance<UnittestComponent>();
 
-      ComponentManager<UnittestComponent>.GetOrCreate(worldId, maxCapacity).Should().NotBeNull();
+      componentManager.GetOrCreate(worldId, maxCapacity).Should().NotBeNull();
+      componentManager.PoolCount.Should().Be(1);
+    }
+    [Fact]
+    public void Remove_ValidWorldIdAndPool_PoolRemoved()
+    {
+      short worldId = 1;
+      int maxCapacity = 1;
+      ComponentManager<UnittestComponent> componentManager = ComponentManager.Instance<UnittestComponent>();
+
+      componentManager.GetOrCreate(worldId, maxCapacity).Should().NotBeNull();
+      componentManager.Remove(worldId).Should().BeTrue();
+      componentManager.PoolCount.Should().Be(0);
     }
   }
 }
--- Valkyr.ECS/ComponentManager.cs
+++ Valkyr.ECS/ComponentManager.cs
@@ -1,15 +1,40 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace Valkyr.ECS
 {
-  internal static class ComponentManager<T>
-    where T : struct
+  internal static class ComponentManager
   {
-    private static readonly ConcurrentDictionary<short, IPool<T>> pools = new();
-
-    internal static IPool<T> GetOrCreate(short worldId, int maxCapacity)
+    private static readonly ConcurrentDictionary<Type, IComponentManager> componentManagers = new();
+    public static ComponentManager<T> Instance<T>()
+      where T : IComponent
     {
-      return pools.GetOrAdd(worldId, new Pool<T>(maxCapacity));
+      return (ComponentManager<T>)componentManagers.GetOrAdd(typeof(T), new ComponentManager<T>());
+    }
+
+    public static void Remove(short worldId)
+    {
+      foreach (KeyValuePair<Type, IComponentManager> entry in componentManagers)
+      {
+        entry.Value.Remove(worldId);
+      }
+    }
+  }
+  internal class ComponentManager<T> : IComponentManager
+      where T : IComponent
+  {
+    private readonly ConcurrentDictionary<short, IComponentPool<T>> pools = new();
+
+    public int PoolCount => pools.Count;
+
+    public IComponentPool<T> GetOrCreate(short worldId, int maxCapacity)
+    {
+      return pools.GetOrAdd(worldId, new ComponentPool<T>(maxCapacity));
+    }
+    public bool Remove(short worldId)
+    {
+      return pools.TryRemove(worldId, out _);
     }
   }
 }
