5793fd5e44f358ea6ed3c93bab7216ea0576f7db
Sebastian Berning <hunter.Vasitas@googlemail.com>
2021-06-19 11:07:07 +0200

    FilterExpression: Adding a new FilterExpression to filter entities based on their components.

--- Valkyr.ECS.Tests/FilterExpressionsTest.cs
+++ Valkyr.ECS.Tests/FilterExpressionsTest.cs
@@ -1,4 +1,6 @@
 ﻿using FluentAssertions;
+using Moq;
+using System;
 using Xunit;
 
 namespace Valkyr.ECS.Tests
@@ -14,6 +16,57 @@ namespace Valkyr.ECS.Tests
       };
       FilterExpressions.All.Matches(ref entities[0]).Should().BeTrue();
       FilterExpressions.All.Matches(ref entities[1]).Should().BeTrue();
+    }
+    [Fact]
+    public void Component_EntitiesHaveComponent_ReturnsTrue()
+    {
+      Mock<IWorld> worldMock = new();
+      Entity[] entities = new Entity[] {
+        new Entity(0, worldMock.Object),
+        new Entity(1, worldMock.Object)
+      };
+
+      worldMock.Setup(_ => _.Has<UnittestComponent>(It.IsAny<int>())).Returns(true);
+
+      FilterExpressions.Component<UnittestComponent>().Matches(ref entities[0]).Should().BeTrue();
+      FilterExpressions.Component<UnittestComponent>().Matches(ref entities[1]).Should().BeTrue();
+    }
+    [Fact]
+    public void Component_ParamNotAnEntity_ThrowsArgumentExeption()
+    {
+      int[] valuesEntities = new int[] { 1 };
+
+      Action action = () => FilterExpressions.Component<UnittestComponent>().Matches(ref valuesEntities[0]);
+
+      action.Should().Throw<ArgumentException>();
+    }
+    [Fact]
+    public void Component_EntitiesHaveNotComponent_ReturnsFalse()
+    {
+      Mock<IWorld> worldMock = new();
+      Entity[] entities = new Entity[] {
+        new Entity(0, worldMock.Object),
+        new Entity(1, worldMock.Object)
+      };
+
+      worldMock.Setup(_ => _.Has<UnittestComponent>(It.IsAny<int>())).Returns(false);
+      FilterExpressions.Component<UnittestComponent>().Matches(ref entities[0]).Should().BeFalse();
+      FilterExpressions.Component<UnittestComponent>().Matches(ref entities[1]).Should().BeFalse();
+    }
+    [Fact]
+    public void Component_MixedEntitiesHaveComponents_ReturnsFalse()
+    {
+      Mock<IWorld> worldMock = new();
+      Entity[] entities = new Entity[] {
+        new Entity(0, worldMock.Object),
+        new Entity(1, worldMock.Object)
+      };
+
+      worldMock.SetupSequence(_ => _.Has<UnittestComponent>(It.IsAny<int>()))
+               .Returns(false)
+               .Returns(true);
+      FilterExpressions.Component<UnittestComponent>().Matches(ref entities[0]).Should().BeFalse();
+      FilterExpressions.Component<UnittestComponent>().Matches(ref entities[1]).Should().BeTrue();
     }
   }
 }
---
+++ Valkyr.ECS/FilterExpressions/ComponentFilterExpression.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Valkyr.ECS
+{
+  internal class ComponentFilterExpression<Component> : IFilterExpression
+    where Component : IComponent
+  {
+    public bool Matches<T>(ref T element)
+    {
+      if (element is not Entity entity)
+        throw new ArgumentException($"Element need to be of type {typeof(Entity)}", nameof(element));
+
+      return entity.Has<Component>();
+    }
+  }
+}
