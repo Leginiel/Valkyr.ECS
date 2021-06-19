dd31fc50e934dd88f761d034f0846bff73e139b7
Sebastian Berning <hunter.Vasitas@googlemail.com>
2021-06-19 11:13:43 +0200

    System: Adding base classes and basic functionality to define systems

---
+++ Valkyr.ECS/System.cs
@@ -0,0 +1,21 @@
+﻿namespace Valkyr.ECS
+{
+  public abstract class System<T> : ISystem
+    where T : struct, IComponent
+  {
+    private readonly IWorld world;
+
+    public bool Enabled { get; set; }
+
+    public System(IWorld world)
+    {
+      this.world = world;
+    }
+
+    protected abstract void Update(ref Entity entity);
+    public void Update()
+    {
+      world.IterateEntities(Update, FilterExpressions.All());
+    }
+  }
+}
